2412c21762a93569cb886b02fef719bea4affb69
Melvin Yeo <melvinyzw@gmail.com>
2019-06-23 13:29:31 +0800

    Update init with new functions

--- animelyrics/__init__.py
+++ animelyrics/__init__.py
@@ -1,3 +1,4 @@
-from .animelyrics import search
+from .animelyrics import search, get_song_info, get_lyrics_soup, get_lyrics_url, InvalidLanguage
 
-__all__ = ['search']
+__all__ = ["search", "get_song_info", "get_lyrics_soup", "get_lyrics_url", "InvalidLanguage"]
+
