d277fda1ea3f9af76dd54c03c95de591d60be5a7
Melvin Yeo <melvinyzw@gmail.com>
2019-06-23 15:12:47 +0800

    Fix imports in init

--- animelyrics/__init__.py
+++ animelyrics/__init__.py
@@ -5,13 +5,16 @@ from .animelyrics import (
     get_lyrics_url,
     InvalidLanguage,
     MissingTranslatedLyrics,
+    NoLyricsFound,
 )
 
 __all__ = [
     "search_lyrics",
     "get_song_info",
+    "get_lyrics_soup",
     "get_lyrics_url",
     "InvalidLanguage",
     "MissingTranslatedLyrics",
+    "NoLyricsFound",
 ]
 
